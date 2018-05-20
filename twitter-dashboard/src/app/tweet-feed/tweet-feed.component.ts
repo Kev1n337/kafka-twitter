a7f672ced359e98135ec98ad602d22126910fe7d
Kevin Linne <linne.kevin@googlemail.com>
2018-05-20 19:29:13 +0200

    Improve dashboard presentation

--- twitter-dashboard/src/app/tweet-feed/tweet-feed.component.ts
+++ twitter-dashboard/src/app/tweet-feed/tweet-feed.component.ts
@@ -10,22 +10,17 @@ import {Tweet} from '../tweet.model';
 })
 export class TweetFeedComponent {
 
-  displayedColumns = ['time', 'name', 'text'];
   displayedTweets: Tweet[] = [];
-  dataSource = new MatTableDataSource(this.displayedTweets);
 
   constructor(private ksql: KsqlService) {
     this.ksql.topicChanged$.subscribe(() => {
-      console.log('Topic changed');
       this.displayedTweets = [];
-      this.dataSource.data = this.displayedTweets;
 
       this.ksql.currentTopic.tweetAdded$.subscribe((tweet: Tweet) => {
         this.displayedTweets.push(tweet);
         if (this.displayedTweets.length > 5) {
           this.displayedTweets.shift();
         }
-        this.dataSource.data = this.displayedTweets;
       });
     });
   }
